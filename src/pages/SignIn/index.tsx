11c52e112b43b4d251939544cdd8912de2a7c5c4
Matheus Martinez <matheus.rmartinez@gmail.com>
2020-10-01 21:45:41 -0300

    'Criação e estilização da tela de login'

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,70 @@
 import React from 'react';
-import { Image } from 'react-native';
-import { Container } from './styles';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  ForgotPassWord,
+  ForgotPassWordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="Email" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('Deu');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassWord
+              onPress={() => {
+                console.log('Alô');
+              }}
+            >
+              <ForgotPassWordText>Esqueci minha senha</ForgotPassWordText>
+            </ForgotPassWord>
+          </Container>
+        </ScrollView>
+
+        <CreateAccountButton onPress={() => {}}>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
